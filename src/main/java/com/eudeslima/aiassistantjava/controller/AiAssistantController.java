fb1265433f3b2a415c2e8d3ff1e4932bd825871e
Eudes Lima <eudesfilho1994@outlook.com>
2024-05-17 09:52:08 -0300

    reestruturação de código para usar LM studio

--- src/main/java/com/eudeslima/aiassistantjava/controller/AiAssistantController.java
+++ src/main/java/com/eudeslima/aiassistantjava/controller/AiAssistantController.java
@@ -2,6 +2,9 @@ package com.eudeslima.aiassistantjava.controller;
 
 import com.eudeslima.aiassistantjava.dto.MessageDTO;
 import com.eudeslima.aiassistantjava.factory.AiAssistantFactory;
+import com.eudeslima.aiassistantjava.factory.ContentRetrieverFactory;
+import com.eudeslima.aiassistantjava.service.DocumentAssistant;
+import com.eudeslima.aiassistantjava.factory.EmbeddingFactory;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/chat")
 public class AiAssistantController {
     @Value("${langchain.huggingface.accessToken}")
-    private String accessToken;
 
     @PostMapping
     public ResponseEntity chat(@RequestBody MessageDTO messageDTO) {
-        ChatLanguageModel chatLanguageModel = AiAssistantFactory.createHuggingFace(accessToken);
-        String response = chatLanguageModel.generate(messageDTO.message());
+        ChatLanguageModel chatModel = AiAssistantFactory.createLocalChatModel();
+        var embeddingModel = EmbeddingFactory.createEmbeddingModel();
+        var embeddingStore = EmbeddingFactory.createEmbeddingStore();
+        var fileContentRetriever = ContentRetrieverFactory.createFileContentRetriever(
+                embeddingModel,
+                embeddingStore,
+                "movies.txt"
+        );
+
+        var documentAssistant = new DocumentAssistant(chatModel, fileContentRetriever);
+        String response = documentAssistant.chat("List all movies with Comedy category");
         return ResponseEntity.ok().body(response);
     }
 }
